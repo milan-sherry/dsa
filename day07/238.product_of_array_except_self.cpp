967acd2f0f87ca4463627b1411d0c5549dab4206
milan-sherry <milan.joseph.sherry@gmail.com>
2026-09-18 19:28:52 +0530

    Create 238.product_of_array_except_self.cpp

---
+++ day07/238.product_of_array_except_self.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> result(nums.size());
+        int prefix=1;
+        int suffix=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            result[i]=prefix;
+            prefix*=nums[i];
+        }
+        for(int i=nums.size()-1;i>-1;i--)
+        {
+            result[i]*=suffix;
+            suffix*=nums[i];
+        }
+        return result;
+    }
+};
